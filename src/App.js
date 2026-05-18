d7a249d25e306e665d65990d2b80de4e4fba55cc
helio-developer <helioendes.es@unitropico.edu.co>
2026-05-18 17:40:13 -0500

    Login

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
 
   const [editandoId, setEditandoId] = useState(null);
 
+  const [usuarioLogin, setUsuarioLogin] = useState("");
+
+  const [passwordLogin, setPasswordLogin] = useState("");
+
 
 
 
@@ -144,11 +148,60 @@ function App() {
   }
 
 
+  async function login() {
+
+    const respuesta = await fetch(
+      "https://developmentfull-backend-api.onrender.com/login",
+      {
+  
+        method: "POST",
+  
+        headers: {
+          "Content-Type": "application/json"
+        },
+  
+        body: JSON.stringify({
+          nombre: usuarioLogin,
+          password: passwordLogin
+        })
+  
+      }
+    );
+  
+    const data = await respuesta.json();
+  
+    console.log(data);
+  
+    localStorage.setItem("token", data.token);
+  
+  }
+
+
 
 
   return (
 
     <div className="container">
+
+<h1>Login</h1>
+
+<input
+  type="text"
+  placeholder="Usuario"
+  value={usuarioLogin}
+  onChange={(e) => setUsuarioLogin(e.target.value)}
+/>
+
+<input
+  type="password"
+  placeholder="Contraseña"
+  value={passwordLogin}
+  onChange={(e) => setPasswordLogin(e.target.value)}
+/>
+
+<button onClick={login}>
+  Iniciar sesión
+</button>
 
 <h1 className="titulo">CRUD Usuarios</h1>
 
